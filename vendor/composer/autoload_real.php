b53bb97335f5ed6ac3a3ffbb80a27b684ef57cd9
alexandru1985 <alexandru_panzaru@yahoo.com>
2024-10-05 15:59:59 +0300

    Add PHPUnit to project

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -31,6 +31,18 @@ class ComposerAutoloaderInit620ce63ecd25bcfacd2f3f5735f44b8d
 
         $loader->register(true);
 
+        $filesToLoad = \Composer\Autoload\ComposerStaticInit620ce63ecd25bcfacd2f3f5735f44b8d::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
+
         return $loader;
     }
 }
